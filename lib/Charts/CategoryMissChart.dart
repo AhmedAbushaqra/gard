d5f23a5cf0e3142ec74493cd573e8df3ec1a01cd
Mohamed.AbuElmagd <mohamed.aboelmagd2012@gmail.com>
2021-03-29 01:53:23 +0200

    Responsiveness

--- lib/Charts/CategoryMissChart.dart
+++ lib/Charts/CategoryMissChart.dart
@@ -37,217 +37,219 @@ class _CategoryMissChartState extends State<CategoryMissChart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            height: 50.0,
-            child: RaisedButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-              padding: EdgeInsets.all(0.0),
-              child: Ink(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [new Color(0xff374ABE), new Color(0xff64B6FF)],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 50.0,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [new Color(0xff374ABE), new Color(0xff64B6FF)],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0)
+                  ),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width*0.5,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.analytics_outlined),
+                        Container(
+                          //constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                          //alignment: Alignment.center,
+                          child: Text(
+                            "Item side of view",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 18,
+                                color: Colors.white
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    borderRadius: BorderRadius.circular(30.0)
+                  ),
                 ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: AspectRatio(
+                aspectRatio: 1,
                 child: Container(
-                  width: MediaQuery.of(context).size.width*0.5,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.analytics_outlined),
-                      Container(
-                        //constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
-                        //alignment: Alignment.center,
-                        child: Text(
-                          "Item side of view",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 18,
-                              color: Colors.white
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: Container(
-                height: MediaQuery.of(context).size.height*0.5,
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-                  color: Colors.white,
-                  child: Padding(
-                    padding:  EdgeInsets.only(top: 16.0,bottom: 35),
-                    child: BarChart(
-                      BarChartData(
-                        alignment: BarChartAlignment.center,
-                        barTouchData: BarTouchData(
-                          touchCallback: (BarTouchResponse barTouchResponse){
-                            setState(() {
-                              if(barTouchResponse.touchInput is FlPanStart && barTouchResponse.spot.touchedBarGroupIndex == 0){
-                                      Navigator.pushNamed(context, ChainMissChart.RouteName);
-                              } else if (barTouchResponse.touchInput is FlPanStart && barTouchResponse.spot.touchedBarGroupIndex == 1){
-                                      Navigator.pushNamed(context, CategoryMissChart.RouteName);
-                              }
-                            });
-                          },
-                          // touchCallback: (barTouchResponse){
-                          //   setState(() {
-                          //     if(touchedIndex == 0){
-                          //       Navigator.pushNamed(context, ChainMissChart.RouteName);
-                          //     } else if (touchedIndex == 1){
-                          //       Navigator.pushNamed(context, CategoryMissChart.RouteName);
-                          //     } else if (touchedIndex == 2){
-                          //       Navigator.pushNamed(context, CategoryMissChart.RouteName);
-                          //     }
-                          //   });
-                          // },
-                          enabled: true,
-                        ),
-                        titlesData: FlTitlesData(
-                          show: true,
-                          bottomTitles: SideTitles(
-                            rotateAngle: 50,
-                            showTitles: true,
-                            getTextStyles: (value) =>
-                                const TextStyle(color: Color(0xff939393), fontSize: 10),
-                            margin: 10,
-                            getTitles: (double value) {
-                              switch (value.toInt()) {
-                                case 0:
-                                  return 'BONJORNO';
-                                case 1:
-                                  return 'NESCAFE';
-                                case 2:
-                                  return 'COFFEE-MATE';
-                                case 3:
-                                  return 'NIDO';
-                                case 4:
-                                  return 'BABY FOOD';
-                                case 5:
-                                  return 'NESQUIK';
-                                case 6:
-                                  return 'MAGGI';
-                                case 7:
-                                  return 'CEREAL';
-                                case 8:
-                                  return 'CONFECTIONERY';
-                                case 9:
-                                  return 'WATER';
-                                default:
-                                  return '';
-                              }
+                  height: MediaQuery.of(context).size.height*0.5,
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+                    color: Colors.white,
+                    child: Padding(
+                      padding:  EdgeInsets.only(top: 16.0,bottom: 35),
+                      child: BarChart(
+                        BarChartData(
+                          alignment: BarChartAlignment.center,
+                          barTouchData: BarTouchData(
+                            touchCallback: (BarTouchResponse barTouchResponse){
+                              setState(() {
+                                if(barTouchResponse.touchInput is FlPanStart && barTouchResponse.spot.touchedBarGroupIndex == 0){
+                                        Navigator.pushNamed(context, ChainMissChart.RouteName);
+                                } else if (barTouchResponse.touchInput is FlPanStart && barTouchResponse.spot.touchedBarGroupIndex == 1){
+                                        Navigator.pushNamed(context, CategoryMissChart.RouteName);
+                                }
+                              });
                             },
+                            // touchCallback: (barTouchResponse){
+                            //   setState(() {
+                            //     if(touchedIndex == 0){
+                            //       Navigator.pushNamed(context, ChainMissChart.RouteName);
+                            //     } else if (touchedIndex == 1){
+                            //       Navigator.pushNamed(context, CategoryMissChart.RouteName);
+                            //     } else if (touchedIndex == 2){
+                            //       Navigator.pushNamed(context, CategoryMissChart.RouteName);
+                            //     }
+                            //   });
+                            // },
+                            enabled: true,
                           ),
-                          leftTitles: SideTitles(
-                            interval: 30,
-                            showTitles: true,
-                            getTextStyles: (value) => const TextStyle(
-                                color: Color(
-                                  0xff939393,
-                                ),
-                                fontSize: 10),
-                            margin: 0,
+                          titlesData: FlTitlesData(
+                            show: true,
+                            bottomTitles: SideTitles(
+                              rotateAngle: 50,
+                              showTitles: true,
+                              getTextStyles: (value) =>
+                                  const TextStyle(color: Color(0xff939393), fontSize: 10),
+                              margin: 10,
+                              getTitles: (double value) {
+                                switch (value.toInt()) {
+                                  case 0:
+                                    return 'BONJORNO';
+                                  case 1:
+                                    return 'NESCAFE';
+                                  case 2:
+                                    return 'COFFEE-MATE';
+                                  case 3:
+                                    return 'NIDO';
+                                  case 4:
+                                    return 'BABY FOOD';
+                                  case 5:
+                                    return 'NESQUIK';
+                                  case 6:
+                                    return 'MAGGI';
+                                  case 7:
+                                    return 'CEREAL';
+                                  case 8:
+                                    return 'CONFECTIONERY';
+                                  case 9:
+                                    return 'WATER';
+                                  default:
+                                    return '';
+                                }
+                              },
+                            ),
+                            leftTitles: SideTitles(
+                              interval: 30,
+                              showTitles: true,
+                              getTextStyles: (value) => const TextStyle(
+                                  color: Color(
+                                    0xff939393,
+                                  ),
+                                  fontSize: 10),
+                              margin: 0,
+                            ),
                           ),
-                        ),
-                        gridData: FlGridData(
-                          show: true,
-                          checkToShowHorizontalLine: (value) => value % 30 == 0,
-                          getDrawingHorizontalLine: (value) => FlLine(
-                            color: const Color(0xffe7e8ec),
-                            strokeWidth: 1,
+                          gridData: FlGridData(
+                            show: true,
+                            checkToShowHorizontalLine: (value) => value % 30 == 0,
+                            getDrawingHorizontalLine: (value) => FlLine(
+                              color: const Color(0xffe7e8ec),
+                              strokeWidth: 1,
+                            ),
                           ),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          groupsSpace: MediaQuery.of(context).orientation == Orientation.portrait ? 20:60,
+                          barGroups: getData(),
                         ),
-                        borderData: FlBorderData(
-                          show: false,
-                        ),
-                        groupsSpace: 20,
-                        barGroups: getData(),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [ Colors.black12, new Color(0xff64B6FF)],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0)
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [ Colors.black12, new Color(0xff64B6FF)],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30.0)
+                    ),
+                    margin: EdgeInsets.all(10),
+                    //width: MediaQuery.of(context).size.width*0.6,
+                    height: 50.0,
+                    child: Row(
+                      children: [
+                        Icon(Icons.arrow_back_ios_outlined ),
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          padding: EdgeInsets.all(10.0),
+                          color: Colors.lightBlue,
+                          textColor: Colors.white,
+                          child: Text("Back to Master",
+                              style: TextStyle(fontSize: 15)),
+                        ),
+                      ],
+                    ),
+                  ),Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [new Color(0xff64B6FF),Colors.black12,],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                        ),
+                        borderRadius: BorderRadius.circular(30.0)
+                    ),
+                    margin: EdgeInsets.all(10),
+                    //width: MediaQuery.of(context).size.width*0.6,
+                    height: 50.0,
+                    child: Row(
+                      children: [
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.lightBlue)),
+                          onPressed: () {
+                          },
+                          padding: EdgeInsets.all(10.0),
+                          color: Color.fromRGBO(0, 160, 227, 1),
+                          textColor: Colors.white,
+                          child: Text("Send",
+                              style: TextStyle(fontSize: 15)),
+                        ),
+                        Icon(Icons.send)
+                      ],
+                    ),
                   ),
-                  margin: EdgeInsets.all(10),
-                  //width: MediaQuery.of(context).size.width*0.6,
-                  height: 50.0,
-                  child: Row(
-                    children: [
-                      Icon(Icons.arrow_back_ios_outlined ),
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        padding: EdgeInsets.all(10.0),
-                        color: Colors.lightBlue,
-                        textColor: Colors.white,
-                        child: Text("Back to Master",
-                            style: TextStyle(fontSize: 15)),
-                      ),
-                    ],
-                  ),
-                ),Container(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [new Color(0xff64B6FF),Colors.black12,],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                      borderRadius: BorderRadius.circular(30.0)
-                  ),
-                  margin: EdgeInsets.all(10),
-                  //width: MediaQuery.of(context).size.width*0.6,
-                  height: 50.0,
-                  child: Row(
-                    children: [
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Colors.lightBlue)),
-                        onPressed: () {
-                        },
-                        padding: EdgeInsets.all(10.0),
-                        color: Color.fromRGBO(0, 160, 227, 1),
-                        textColor: Colors.white,
-                        child: Text("Send",
-                            style: TextStyle(fontSize: 15)),
-                      ),
-                      Icon(Icons.send)
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
