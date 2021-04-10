ed5f85e16835d83ea5cba5d0b957a700ba799478
abushaqra <ahmedhossin1122@gmail.com>
2021-04-10 18:37:47 +0200

    554654664

--- lib/Charts/CategoryMissChart.dart
+++ lib/Charts/CategoryMissChart.dart
@@ -212,6 +212,7 @@ class _CategoryMissChartState extends State<CategoryMissChart> {
                             side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
                         onPressed: () {
                           Navigator.of(context).pop();
+                          Navigator.of(context).pop();
                         },
                         padding: EdgeInsets.all(10.0),
                         color: Colors.lightBlue,
