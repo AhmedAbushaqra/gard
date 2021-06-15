385aa40fcc4b9d0f28fd4267b3990385a30f6e26
Mohamed Abu Elmagd <mohamed.aboelmagd2021@gmail.com>
2021-06-15 16:48:57 +0200

    generate command of localization again

---
+++ lib/translation/codegen_loader.g.dart
@@ -0,0 +1,80 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ar = {
+  "location": "الموقع",
+  "AlexAndDelta": "اسكندريه و دلتا",
+  "Cairo": "قاهره",
+  "LogIn": "دخول",
+  "Carrfour": "كارفور",
+  "CarrfourExpress": "كارفور اكسبريس",
+  "hipperOne": "هايبر وان",
+  "Spinnyes": "سبينيس",
+  "Seoudi": "سعودى",
+  "RagabSons": "اولاد رجب",
+  "Metro": "مترو",
+  "KherZaman": "خير زمان",
+  "ELOtheim": "العثيم",
+  "raya": "الرايه",
+  "alfa": "ألفا",
+  "Elmahalawy": "المحلاوى",
+  "panda": "باندا",
+  "Elhawary": "الهوارى",
+  "oscar": "اوسكار",
+  "lulu": "لولو",
+  "FAthallaGomla": "فتح الله جمله",
+  "BeitElgomal": "بيت الجمله",
+  "AswaqFathalla": "اسواق فتح الله",
+  "AswaqFathallamini": "اسواق فتح الله مينى",
+  "Zahran": "زهران",
+  "freshfood": "فريش فوود",
+  "RoyalHouse": "رويال هاوس",
+  "Martville": "مارت فييل",
+  "Premier": "بريمير"
+};
+static const Map<String,dynamic> en = {
+  "location": "Location",
+  "AlexAndDelta": "Alex&Delta",
+  "Cairo": "Cairo",
+  "LogIn": "LogIn",
+  "Carrfour": "Carrefour",
+  "CarrfourExpress": "Carrefour Express",
+  "hipperOne": "Hipper one",
+  "Spinnyes": "Spinneys",
+  "Seoudi": "Seoudi",
+  "RagabSons": "Ragab sons",
+  "Metro": "Metro",
+  "KherZaman": "Kher zaman",
+  "ELOtheim": "El otheim",
+  "raya": "Raya",
+  "alfa": "Alfa",
+  "Elmahalawy": "El Mahalawy",
+  "panda": "Panda",
+  "Elhawary": "El Hawary",
+  "oscar": "Oscar",
+  "lulu": "Lulu",
+  "FAthallaGomla": "Fathalla Gomla",
+  "BeitElgomal": "Beit el gomla",
+  "AswaqFathalla": "Aswaq Fathalla",
+  "AswaqFathallamini": "Aswak fathalla mini",
+  "Zahran": "Zahran",
+  "freshfood": "Fresh food",
+  "RoyalHouse": "Royal house",
+  "Martville": "Mart ville",
+  "Premier": "Premier"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar": ar, "en": en};
+}
---
+++ lib/translation/locale_keys.g.dart
@@ -0,0 +1,34 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const location = 'location';
+  static const AlexAndDelta = 'AlexAndDelta';
+  static const Cairo = 'Cairo';
+  static const LogIn = 'LogIn';
+  static const Carrfour = 'Carrfour';
+  static const CarrfourExpress = 'CarrfourExpress';
+  static const hipperOne = 'hipperOne';
+  static const Spinnyes = 'Spinnyes';
+  static const Seoudi = 'Seoudi';
+  static const RagabSons = 'RagabSons';
+  static const Metro = 'Metro';
+  static const KherZaman = 'KherZaman';
+  static const ELOtheim = 'ELOtheim';
+  static const raya = 'raya';
+  static const alfa = 'alfa';
+  static const Elmahalawy = 'Elmahalawy';
+  static const panda = 'panda';
+  static const Elhawary = 'Elhawary';
+  static const oscar = 'oscar';
+  static const lulu = 'lulu';
+  static const FAthallaGomla = 'FAthallaGomla';
+  static const BeitElgomal = 'BeitElgomal';
+  static const AswaqFathalla = 'AswaqFathalla';
+  static const AswaqFathallamini = 'AswaqFathallamini';
+  static const Zahran = 'Zahran';
+  static const freshfood = 'freshfood';
+  static const RoyalHouse = 'RoyalHouse';
+  static const Martville = 'Martville';
+  static const Premier = 'Premier';
+
+}
